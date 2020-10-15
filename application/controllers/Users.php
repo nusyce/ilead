050bcab3ddcf3d29f09af33c80c4bf4c1fb1ae2c
nusyce <nusyce@gmail.com>
2020-10-15 19:21:23 +0100

    Update

---
+++ application/controllers/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require 'AdminControler.php';
+
+class Users extends AdminControler
+{
+
+    public function index()
+    {
+        $this->load_view('user/manager');
+    }
+}
--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
+    <title>iLEAD</title>
     <!-- core:css -->
     <link rel="stylesheet" href="<?= base_url()?>assets/vendors/core/core.css">
     <!-- endinject -->
     <!-- plugin css for this page -->
     <link rel="stylesheet" href="<?= base_url()?>assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+
     <!-- end plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="<?= base_url()?>assets/fonts/feather-font/css/iconfont.css">
@@ -22,3 +24,5 @@
 </head>
 <body>
 <div class="main-wrapper">
+
+    <div class="page-wrapper">

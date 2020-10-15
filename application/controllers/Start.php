feeaf0e970e1ea3978c8f397e2517177a8c173c6
nusyce <nusyce@gmail.com>
2020-10-15 19:27:22 +0100

    updayte

--- application/controllers/Start.php
+++ application/controllers/Start.php
@@ -1,10 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 require 'AdminControler.php';
+
 class Start extends AdminControler
 {
 
     public function index()
+    {
+        $this->load->view('home/home');
+    }
+    
+
+    public function dashboard()
     {
         $this->load_view('dashboard/dashboard');
     }
